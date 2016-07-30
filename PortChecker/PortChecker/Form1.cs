a3e66c5805fd1b745f7a86592ec7c78065cc2825
jmydde <jamesmyddelton@gmail.com>
2016-07-30 11:38:58 +0100

    Test

--- PortChecker/PortChecker/Form1.cs
+++ PortChecker/PortChecker/Form1.cs
@@ -8,6 +8,9 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// Additional
+using System.Net.Sockets;
+
 namespace PortChecker
 {
     public partial class Form1 : Form
@@ -15,6 +18,45 @@ namespace PortChecker
         public Form1()
         {
             InitializeComponent();
+        }
+        bool IsPortOpen(string host, int port, TimeSpan timeout)
+        {
+            try
+            {
+                using (var client = new TcpClient())
+                {
+                    var result = client.BeginConnect(host, port, null, null);
+                    var success = result.AsyncWaitHandle.WaitOne(timeout);
+                    if (!success)
+                    {
+                        return false;
+                    }
+
+                    client.EndConnect(result);
+                }
+
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TimeSpan span = new TimeSpan(1, 2, 0, 30, 0);
+
+            var isopen = IsPortOpen("localhost", 81, span);
+
+            if (isopen)
+            {
+                button1.BackColor = Color.Green;
+            }
+            else
+            {
+                button1.BackColor = Color.Red;
+            }
         }
     }
 }
